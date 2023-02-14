0296853c0bf5eca5c555ca56fda92a4551b12f7e
CF <captainfedora@skillup.tech>
2023-02-14 11:24:05 -0800

    trying

---
+++ app.py
@@ -0,0 +1,28 @@
+'''
+Created on 04-Sep-2019
+
+@author: bkadambi
+'''
+
+# -*- coding: UTF-8 -*-
+"""
+hello_flask: First Python-Flask webapp
+"""
+from flask import Flask  # From module flask import class Flask
+from pymongo import MongoClient
+app = Flask(__name__)    # Construct an instance of Flask class for our webapp
+
+client = MongoClient('mongodb://%s:%s@my-mongodb' % ('myuser', 'mypassword'))
+
+# print(client.tododb.list_collection_names())
+for db in client.list_databases():
+    print(db)
+
+@app.route('/')   # URL '/' to be handled by main() route handler
+def main():
+    """Say hello"""
+    return 'Hello, world!'
+
+if __name__ == '__main__':  # Script executed directly?
+    print("Hello, World. Uses S2I to build the application.")
+    app.run(host="0.0.0.0", port=8080, debug=True,use_reloader=True)  # Launch built-in web server and run this Flask webapp
